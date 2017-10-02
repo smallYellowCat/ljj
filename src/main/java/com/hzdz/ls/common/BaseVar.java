a57565fc02f8e5bdc58c1c86058f5ebbe3bb60e8
doudouou <dreamdeveloper@126.com>
2017-10-02 22:07:22 +0800

    新增图片处理工具类处理方法

--- src/main/java/com/hzdz/ls/common/BaseVar.java
+++ src/main/java/com/hzdz/ls/common/BaseVar.java
@@ -1,5 +1,5 @@
 package com.hzdz.ls.common;
 
 public class BaseVar {
-    public static  final String  BASE_URL = "http://10.100.200.201:8080/ljj";
+    public static  final String  BASE_URL = "http://192.168.0.103：8080/ljj";
 }
--- src/main/java/com/hzdz/ls/common/ImageUtil.java
+++ src/main/java/com/hzdz/ls/common/ImageUtil.java
@@ -8,7 +8,9 @@ import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.awt.image.RenderedImage;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -26,7 +28,7 @@ public class ImageUtil {
     }
 
     /**
-     * 图片添加边框
+     * 图片打水印，文字
      * @param path
      * @param frameModePath
      * @return
@@ -55,6 +57,72 @@ public class ImageUtil {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+
+    /**
+     * 两张图片进行合并
+     * @param image1
+     * @param image2
+     * @return
+     */
+    public static String mergeImage(String image1, String image2) throws IOException {
+        BufferedImage bi1 = ImageIO.read(new File(image1));
+        BufferedImage bi2 = ImageIO.read(new File(image2));
+        Graphics2D g1 = (Graphics2D) bi1.getGraphics();
+
+        int startX = (bi1.getWidth() - bi2.getWidth()) / 2;
+        int startY = (bi1.getHeight() - bi2.getHeight()) / 2;
+
+        g1.drawImage(bi2, startX, startY,  bi2.getWidth(), bi2.getHeight(), null);
+        g1.dispose();
+        ImageIO.write(bi1, "jpg", new FileOutputStream(new File("E://cc.jpg")));
+        return null;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        //自动生成main
+        mergeImage("E:/IntelijIdea_workspace/ljj/target/ljj/upload/manager/bbb/20170929145354535.jpg",
+                "E://dd.jpg");
+        //equalRatioScale("E:/IntelijIdea_workspace/ljj/target/ljj/upload/manager/bbb/20170929145354535.jpg", 50, 100);
+    }
+
+    /**
+     * 图片等比例缩放
+     * @param path
+     * @return
+     */
+    public static String equalRatioScale(String path, int tw, int th) throws IOException {
+
+
+        BufferedImage bi = ImageIO.read(new File(path));
+        BufferedImage target = null;
+        int type = bi.getType();
+        double sx = tw / 1.0 / bi.getWidth();
+        double sy = th / 1.0 / bi.getHeight();
+
+        if (sx < sy){
+            sx = sy;
+            tw = (int) (sx * bi.getWidth());
+        }else {
+            sy = sx;
+            th = (int) (sy * bi.getHeight());
+        }
+
+        if (type == BufferedImage.TYPE_CUSTOM) { // handmade
+            ColorModel cm = bi.getColorModel();
+            WritableRaster raster = cm.createCompatibleWritableRaster(tw, th);
+            boolean alphaPremultiplied = cm.isAlphaPremultiplied();
+            target = new BufferedImage(cm, raster, alphaPremultiplied, null);
+        } else {
+            target = new BufferedImage(tw, th, type);
+            Graphics2D g = target.createGraphics();
+            // smoother than exlax:
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+            g.drawRenderedImage(bi, AffineTransform.getScaleInstance(sx, sy));
+            g.dispose();
+        }
+        ImageIO.write(target, "jpg", new FileOutputStream(new File("E://dd.jpg")));
         return null;
     }
 
