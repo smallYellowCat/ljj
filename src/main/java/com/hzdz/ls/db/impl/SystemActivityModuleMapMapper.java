153387aa86d314ed8013aab4af1872ca1cbfd17e
doudouou <dreamdeveloper@126.com>
2017-10-31 14:44:25 +0800

    新增根据活动id查询所拥有模块信息

--- src/main/java/com/hzdz/ls/db/impl/SystemActivityModuleMapMapper.java
+++ src/main/java/com/hzdz/ls/db/impl/SystemActivityModuleMapMapper.java
@@ -1,7 +1,17 @@
 package com.hzdz.ls.db.impl;
 
+import com.hzdz.ls.db.entity.SystemModule;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SystemActivityModuleMapMapper {
+
+    /**
+     * 通过活动id查询当前活动拥有的所有模块
+     * @param activityId
+     * @return
+     */
+    List<SystemModule> queryModuleIdsById(int activityId);
 }
