fe21330c24cf711a5e8a05dced9fd450ba77902c
doudouou <dreamdeveloper@126.com>
2017-11-13 11:46:23 +0800

    新增设备码长度

--- src/main/java/com/hzdz/ls/common/BaseVar.java
+++ src/main/java/com/hzdz/ls/common/BaseVar.java
@@ -4,4 +4,5 @@ package com.hzdz.ls.common;
 public class BaseVar {
     public static  final String  BASE_URL = "101.132.147.167/ljj";
     public static final long SESSION_OUTTIME = 2 * 60 * 60 * 1000;//session失效时间两小时
+    public static final int DID_LENGTH = 16;
 }
