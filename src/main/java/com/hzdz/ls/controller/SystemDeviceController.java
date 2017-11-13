0adaeea8bef6c416742f2ee899a1b04b6d53c751
doudouou <dreamdeveloper@126.com>
2017-11-13 11:46:43 +0800

    新增设备管理相关方法

--- src/main/java/com/hzdz/ls/controller/SystemDeviceController.java
+++ src/main/java/com/hzdz/ls/controller/SystemDeviceController.java
@@ -1,10 +1,12 @@
 package com.hzdz.ls.controller;
 
 import com.hzdz.ls.common.Result;
+import com.hzdz.ls.intercepter.MyIntercepter;
 import com.hzdz.ls.service.SystemDeviceServer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/systemDevice")
@@ -13,7 +15,36 @@ public class SystemDeviceController {
     @Autowired
     private SystemDeviceServer systemDeviceServer;
 
-    public Result inserDevice(){
-        return  null;
+    /**
+     * 新增设备
+     * @param DID 设备号
+     * @return
+     */
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public Result inserDevice(@RequestParam String DID){
+        return  systemDeviceServer.addDevice(DID);
+    }
+
+    /**
+     * 设备列表查看
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Result list(HttpServletRequest request){
+        return systemDeviceServer.list(request);
+    }
+
+    /**
+     * 删除设备
+     * @param id 主键id
+     * @return
+     */
+    @RequestMapping(value="delete", method = RequestMethod.POST)
+    @ResponseBody
+    public Result delete(int id){
+        return systemDeviceServer.deleteByid(id);
     }
 }
