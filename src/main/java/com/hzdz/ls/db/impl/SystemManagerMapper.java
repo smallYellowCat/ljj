16ebcec12f240c2cf14940123383c59b2396e625
smallYellocat <dreamdeveloper@126.com>
2017-11-13 11:57:29 +0800

    暂时提交

--- src/main/java/com/hzdz/ls/db/impl/SystemManagerMapper.java
+++ src/main/java/com/hzdz/ls/db/impl/SystemManagerMapper.java
@@ -4,11 +4,14 @@ import com.hzdz.ls.db.entity.SystemManager;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SystemManagerMapper {
     int isRepeat(@Param("userAccount") String userAccount);
     int addNewManager(SystemManager systemManager);
     int updatePassword(SystemManager systemManager);
     int frozenManager(SystemManager systemManager);
-    SystemManager getManagerByID(int id);
+    int resetPassword(Integer id);
+    List<SystemManager> selectAllManager();
 }
