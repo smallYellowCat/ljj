f74cf1dc4867aa17a68f28bbfcaadcfda7a6b85e
开心的豆豆 <dreamdeveloper@126.com>
2020-08-15 13:30:27 +0800

    删除敏感信息防止泄露

--- src/main/java/com/hzdz/ls/common/SMSUtil.java
+++ src/main/java/com/hzdz/ls/common/SMSUtil.java
@@ -20,8 +20,8 @@ public class SMSUtil {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "LTAILA19DJa2F8dP";
-    static final String accessKeySecret = "zbs03Etjgr5WmCh5HU2N1VUrPzxYUE";
+    static final String accessKeyId = "xxx";
+    static final String accessKeySecret = "xxx";
 
     private static SendSmsResponse sendSms(String code) throws ClientException {
 
@@ -39,9 +39,9 @@ public class SMSUtil {
         //必填:待发送手机号
         request.setPhoneNumbers("18768466004");
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("豆豆");
+        request.setSignName("xx");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("SMS_100220026");
+        request.setTemplateCode("xxx");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         request.setTemplateParam("{\"name\":\"Tom\", \"code\":\""+code+"\"}");
 
